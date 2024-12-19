6c22b5f4f67ade184a917dedb4e823f1710382b6
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2024-12-19 13:19:26 +0530

    Adding Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## Creating a course Selling App
+
+- Creating routes
+- Creating database and Schema for User, Admin, Purchase, courses
+- Writing logic for backend
+- Writing frontend
---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const{ Router }= require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req, res){
+    res.json({
+        message: "You have signed up!"
+    })
+})
+
+adminRouter.post("/sigin", function(req, res){
+    res.json({
+        message: "You have logged in!"
+    })
+})
+
+adminRouter.post("/course", function(req, res){
+
+})
+
+adminRouter.put("/course", function(req, res){
+
+})
+
+adminRouter.get("/course/bulk", function(req, res){
+
+})
+
+module.exports= {
+    adminRouter: adminRouter
+}
